431747c356dc5aea6232ee65dd99feff24ecd8c0
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-05-29 20:04:42 +0900

    [BOJ]전체계산 /브론즈3 /10분

--- ho1121h/[BOJ]전체계산/[BOJ]전체계산.py
+++ ho1121h/[BOJ]전체계산/[BOJ]전체계산.py
@@ -1,7 +1,7 @@
 
 n, m =map(int,input().split())
 a =n #100
-while n // m :#  100 8 = 12   / 12 8 = 1
+while n // m :# 나눔이 가능할때 까지 반복
     a  += n//m # 100+ 12 =112 /  112 + 1 = 113
     n //= m #12               /  1
 print(a) #최종 a = 113
