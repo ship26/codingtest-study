c90363cf55d33ccd8939237720db80dd6cc60811
sylee3000 <102509777+sylee3000@users.noreply.github.com>
2022-06-11 13:35:57 +0900

    Delete sylee3000/220610~220613/[BOJ] 15829_Hashing directory

--- sylee3000/220610~220613/[BOJ] 15829_Hashing/[BOJ] 15829_Hashing.py
+++
@@ -1,8 +0,0 @@
-L = int(input())
-string = input()
-result = 0
-index = 0
-for i in string:
-    result += ((ord(i)-ord('a')+1)*pow(31, index)) % 1234567891
-    index += 1
-print(result % 1234567891)
