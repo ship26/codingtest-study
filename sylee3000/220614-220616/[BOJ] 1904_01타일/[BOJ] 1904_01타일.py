48d5434f4617904194e35ee3a6c00e12a59ba032
sylee3000 <102509777+sylee3000@users.noreply.github.com>
2022-06-16 19:53:24 +0900

    [BOJ] 1904_01타일 / 실버3 / 3분

--- sylee3000/220614-220616/[BOJ] 1904_01타일/[BOJ] 1904_01타일.py
+++ sylee3000/220614-220616/[BOJ] 1904_01타일/[BOJ] 1904_01타일.py
@@ -1,8 +1,8 @@
 N = int(input())
 
-pinary = [0, 1, 2]
+tiles_01 = [0, 1, 2]
 
 if N >= 3:
     for i in range(3, N + 1):
-        pinary.append((pinary[i-2] + pinary[i-1]) % 15746) 
-print(pinary[N])
+        tiles_01.append((tiles_01[i-2] + tiles_01[i-1]) % 15746) 
+print(tiles_01[N])
