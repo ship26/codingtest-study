1e403251cb1bb1152583584b7b55814fdb491d6a
sylee3000 <102509777+sylee3000@users.noreply.github.com>
2022-06-19 05:25:50 +0900

    [BOJ] 10610_30 / 실버5 / 5분

--- sylee3000/220617-220620/[BOJ] 10610_30/[BOJ] 10610_30.py
+++ sylee3000/220617-220620/[BOJ] 10610_30/[BOJ] 10610_30.py
@@ -9,9 +9,7 @@ for i in N:
 
 if total_sum % 3 == 0 and zero_count > 0:
     N = sorted(N, reverse=True)
-    for i in range(len(N) - zero_count):
+    for i in range(len(N)):
         print(N[i], end='')
-    for i in range(zero_count):
-        print(0, end='')
 else:
     print(-1)
