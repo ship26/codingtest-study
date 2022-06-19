fd01f6b6b061201576a420c7c591fb93d084d30e
sylee3000 <102509777+sylee3000@users.noreply.github.com>
2022-06-19 23:06:43 +0900

    [BOJ] 1463_1로 만들기 / 실버3 / 1시간

--- sylee3000/220617-220620/[BOJ] 1463_1로 만들기/[BOJ] 1463_1로 만들기.py
+++ sylee3000/220617-220620/[BOJ] 1463_1로 만들기/[BOJ] 1463_1로 만들기.py
@@ -1,8 +1,8 @@
 N = int(input())
-dp = [-1, 0, 1, 1]
+dp = [-1, 0]
 
-if N > 3:
-    for i in range(4, N + 1):
+if N > 1:
+    for i in range(2, N + 1):
         if i % 2 == 0 and i % 3 == 0:
             dp.append(min(dp[i // 2], dp[i // 3], dp[i - 1]) + 1)
         elif i % 2 == 0:
