3b8e5e88c8ae50f5d3d3dabe96f05de1ff873734
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-06-20 19:53:57 +0900

    [BOJ]더하기 사이클 / 브론즈 1 / 30분 / fix

--- ho1121h/6-20/[BOJ] 더하기 사이클/[BOJ] 더하기 사이클.py
+++ ho1121h/6-20/[BOJ] 더하기 사이클/[BOJ] 더하기 사이클.py
@@ -1,5 +1,4 @@
-
-
+''' 시간초과
 n = input() #문자열로 받음
 m = n
 a = 0
@@ -13,4 +12,23 @@ while 1:
 
     if m == n :
         print(a)
-        break # 반복문 탈출
+        break # 반복문 탈출'''
+
+
+N =str(input())
+RN = int(N)
+N2 = None # 빈값
+
+if int(N) <10 :
+    N='0' + N
+cnt  = 0
+
+while N2!=RN:
+    R=int(N[0])+int(N[1])
+    R = str(R)
+    if int(R) <10:
+        R ='0' + R
+    N = N[1]+R[1]
+    N2 = int(N)
+    cnt +=1
+print(cnt)
