a35abe28b9b71d791cd92fc9443cb117bb43f93a
summer <4003summer@gmail.com>
2022-06-27 18:41:20 +0900

    [ETC] 정수 3개 입력받아 짝/홀 출력하기 / 파이썬 기초 100제 / 1분

---
+++ 403summer/CodeUp/[ETC]_6066/[ETC]_6066.py
@@ -0,0 +1,21 @@
+#[기초-선택실행구조] 정수 3개 입력받아 짝/홀 출력하기
+
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a%2==0:
+    print('even')
+else:
+    print('odd')
+
+if b%2==0:
+    print('even')
+else:
+    print('odd')
+
+if c%2==0:
+    print('even')
+else:
+    print('odd')    
